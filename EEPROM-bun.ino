38a313ecdb8feebeb7fd90dfb108d4035a09f2aa
Silview96 <37659700+Silview96@users.noreply.github.com>
2018-04-11 17:20:37 +0300

    Add files via upload

---
+++ EEPROM-bun.ino
@@ -0,0 +1,39 @@
+#include <Time.h>
+#include <TimeLib.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <EEPROM.h>
+//I2C pins declaration
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
+
+String m;
+String r;
+ int eeAddress = 0;
+void setup() 
+{
+Serial.begin(9600);
+lcd.begin(16,2);//Defining 16 columns and 2 rows of lcd display
+lcd.backlight();//To Power ON the back light
+
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
+}
+ 
+void loop() 
+{
+    delay(1000);
+    while(Serial.available())
+    {
+      m=Serial.readStringUntil('\0');
+      EEPROM.put(0,m);
+    }
+EEPROM.get(0,r);
+lcd.setCursor(0,0);
+lcd.print(r);
+
+}
+
+
+
+
